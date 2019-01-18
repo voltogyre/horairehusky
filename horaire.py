cb677d04188763cf0b066b6c6b23aa165bec63e5
Alexandre Roger <alexandre.roger@lacapitale.com>
2019-01-18 18:45:14 -0500

    update

--- horaire.py
+++ horaire.py
@@ -1,53 +1,37 @@
 import json
 import os
 
+def xstr(s):
+    if s is None:
+        return ''.encode('utf-8')
+    return str(s).encode('utf-8')
+
 with open(os.path.dirname(os.path.abspath(__file__))+'/outputpretty.json', 'r') as fp:
     obj = json.load(fp)
 fp.close
-file = open(os.path.dirname(os.path.abspath(__file__)) + "/NoviceB1.txt","w")
-file.write( "************************************************************\n")
-file.write( "                        Novice B1 Majeur\n")
-file.write( "************************************************************\n")
+teamlist = {}
+#print("json loaded")
 for event in obj:
     for teams in event["TeamList"]:
-        if  teams["Name"].encode('utf-8') == "Novice B1 Majeur" :
-            file.write( "=============================\n")
-            file.write( event["Title"].encode('utf-8') + "\n")
-            file.write( event['Date'].encode('utf-8') + "\n")
-            file.write( event['StartTime'].encode('utf-8') + " - " + event['EndTime'].encode('utf-8') + " ( " +  event['Duration'].encode('utf-8') + " )" + "\n")
-            file.write( event['SportCenterAbr'].encode('utf-8') + " - " + event['SportCenterName'].encode('utf-8') + "\n")
-            for teams in event["TeamList"]:
-                file.write( "    " + teams["Name"].encode('utf-8') + "\n")
-file.close
-file = open(os.path.dirname(os.path.abspath(__file__)) + "/PeeweeA1.txt","w")
-file.write( "************************************************************" + "\n")
-file.write( "                        Peewee A1" + "\n")
-file.write( "************************************************************" + "\n")
-for event in obj:
-    for teams in event["TeamList"]:
-        if  teams["Name"].encode('utf-8') == "Peewee A1":
-            file.write( "=============================" + "\n")
-            file.write( event["Title"].encode('utf-8') + "\n")
-            file.write( event['Date'].encode('utf-8') + "\n")
-            file.write( event['StartTime'].encode('utf-8') + " - " + event['EndTime'].encode('utf-8') + " ( " +  event['Duration'].encode('utf-8') + " )" + "\n")
-            file.write( event['SportCenterAbr'].encode('utf-8') + " - " + event['SportCenterName'].encode('utf-8') + "\n")
-            for teams in event["TeamList"]:
-                file.write( "    " + teams["Name"].encode('utf-8') + "\n")
+        #print(teams["Name"].encode('utf-8'))
+        teamlist[teams["Name"].encode('utf-8')] = 1
 
-file.close
-
-file = open(os.path.dirname(os.path.abspath(__file__)) + "/Dispo.txt","w")
-file.write( "************************************************************" + "\n")
-file.write( "                        Glace Dispo" + "\n")
-file.write( "************************************************************" + "\n")
-for event in obj:
-        if  event["Description"].encode('utf-8') == "Glace disponible":
-            file.write( "=============================" + "\n")
-            file.write( event["Title"].encode('utf-8') + "\n")
-            file.write( event['Date'].encode('utf-8') + "\n")
-            file.write( event['StartTime'].encode('utf-8') + " - " + event['EndTime'].encode('utf-8') + " ( " +  event['Duration'].encode('utf-8') + " )" + "\n")
-            file.write( event['SportCenterAbr'].encode('utf-8') + " - " + event['SportCenterName'].encode('utf-8') + "\n")
-            for teams in event["TeamList"]:
-                file.write( "    " + teams["Name"].encode('utf-8') + "\n")
-
-file.close
+#print("teams loaded")
+for team in teamlist :
+    #print(team)
+    file = open(os.path.dirname(os.path.abspath(__file__)) + "/"+ team +".txt","w")
+    file.write( "************************************************************\n")
+    file.write( "                        "+ team +"\n")
+    file.write( "************************************************************\n")
+    for event in obj:
+        #print(event)
+        for teams in event["TeamList"]:
+            if  teams["Name"].encode('utf-8') == team :
+                file.write( "=============================\n")
+                file.write( event["Title"].encode('utf-8') + "\n")
+                file.write( event['Date'].encode('utf-8') + "\n")
+                file.write( xstr(event['StartTime']) + " - " + xstr(event['EndTime']) + " ( " +  xstr(event['Duration']) + " )" + "\n")
+                file.write( event['SportCenterAbr'].encode('utf-8') + " - " + event['SportCenterName'].encode('utf-8') + "\n")
+                for teams in event["TeamList"]:
+                    file.write( "    " + teams["Name"].encode('utf-8') + "\n")
+    file.close
